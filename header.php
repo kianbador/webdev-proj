2347e7e64d46c550a0575fa76a9499c145f1d806
Kian Bador <106530735+kianbador@users.noreply.github.com>
2024-02-08 15:19:38 +0800

    added header

---
+++ header.php
@@ -0,0 +1,61 @@
+
+
+<html lang="en">
+<head>
+    <meta charset="UTF-8">
+    <meta name="viewport" content="width=device-width, initial-scale=1.0">
+    <link rel="stylesheet" href="https://cdn.jsdelivr.net/npm/bootstrap-icons@1.11.3/font/bootstrap-icons.min.css">
+    <link href="https://cdn.jsdelivr.net/npm/bootstrap@5.3.2/dist/css/bootstrap.min.css" rel="stylesheet">
+    <link rel="stylesheet" href="styles.css">
+    <link rel="icon" href="img/pup.png" type="image/x-icon">
+    <title class="">Application for New/Replacement ID</title>
+</head>
+<body>
+    <nav class="navbar navbar-expand-sm">
+        <div class="container-fluid">
+            <ul class="navbar-nav">
+                <li class="nav-item">
+                    <a class="nav-link active p-0" href=""><img src="img/nav-logo.png" alt="Image" class="img-thumbnail ms-3 me-5" width="180" height="60"></a>
+                </li>
+
+                <li class="nav-item me-5">
+                    <a href="" class="text-warning terms nav-link p-0 ms-4 mt-5"><i class="bi bi-house-fill"></i> Home</a>
+                </li>
+
+                <li class="nav-item me-5">
+                    <a href="" class="text-warning terms nav-link p-0 ms-3 mt-5"><i class="bi bi-info-circle-fill"></i> About Us</a>
+                </li>
+
+                <li class="nav-item me-5">
+                    <a href="" class="text-warning terms nav-link p-0 ms-3 mt-5"><i class="bi bi-mortarboard-fill"></i> Academic</a>
+                </li>
+
+                <li class="nav-item me-5">
+                    <a href="" class="text-warning terms nav-link p-0 ms-3 mt-5"><i class="bi bi-person-lines-fill"></i> Student</a>
+                </li>
+
+                <li class="nav-item me-5">
+                    <a href="" class="text-warning terms nav-link p-0 ms-3 mt-5"><i class="bi bi-search"></i> Research</a>
+                </li>
+                
+                <li class="nav-item me-5">
+                    <a href="" class="text-warning terms nav-link p-0 ms-3 mt-5"><i class="bi bi-telephone-fill"></i> Contact Us</a>
+                </li>
+
+                <li class="nav-item">
+                    <div class="dropdown mt-3">
+                        <button class="btn btn-lg btn-outline-light dropdown-toggle text-light">
+                            <i class="bi bi-person-circle"></i>
+                        </button>
+                        <ul class="dropdown-menu text-center">
+                            <li><a class="dropdown-item btn btn-outline-dark" href="">Profile</a></li>
+                            <li><a class="dropdown-item mt-2 btn btn-outline-dark" href="">Appointment</a></li>
+                            <li><a class="btn btn-outline-danger mt-2" href="">Log out</a></li>
+                        </ul>
+                    </div>
+                </li>
+            </ul>
+        </div>
+    </nav>
+</body>
+</html>
